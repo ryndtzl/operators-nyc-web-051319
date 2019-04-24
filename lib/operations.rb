9e49865f11b4532aaed9c6576e329d8419433331
Ryan Ditzel <ryand370@gmail.com>
2019-04-24 20:04:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,4 @@
 def unsafe?(speed)
-  speed < 40 || speed > 60 ?  true : false
-end
-
-
-
-def not_safe?(speed)
   if speed < 40
     true
   elsif speed > 60
@@ -12,7 +6,11 @@ def not_safe?(speed)
   else
     false
   end
-  
+end
+
+
+
+def not_safe?(speed)
   speed < 40 || speed > 60 ?  true : false
 end
 	
